6db9b8faf1fe82c4c93751d0af7ce4c653755b8c
unknown <adeyemioluwakayode73@gmail.com>
2022-02-03 07:19:59 +0100

    my code

---
+++ html 5 and css practice codes/new 1.c
@@ -0,0 +1,4 @@
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0" nonce="Qjv5TIoy"></script>
+
+<div class="fb-share-button" data-href="https://web.facebook.com/chinedum.ofili" data-layout="button_count" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fweb.facebook.com%2Fchinedum.ofili&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
---
+++ html 5 and css practice codes/new 1.py
@@ -0,0 +1,21 @@
+<div id="disqus_thread"></div>
+<script>
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://http-44-192-114-75.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<script id="dsq-count-scr" src="//http-44-192-114-75.disqus.com/count.js" async></script>
+http://192.168.4.1/login?dst=http%3A%2F%2Fwww.msftconnecttest.com%2Fredirect
