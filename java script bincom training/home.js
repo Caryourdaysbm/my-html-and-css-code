e6923e51aeb354027283879409d25f6735b36694
unknown <adeyemioluwakayode73@gmail.com>
2022-02-21 21:42:59 +0100

    update

---
+++ java script bincom training/home.js
@@ -0,0 +1,51 @@
+//console.log('hello');
+//alert('hello this is kay');
+var b = 'smoothie';
+console.log(b);
+ var someNumber = 45;
+ console.log(someNumber);
+  
+
+ //var age = prompt('What is your age?');
+ //document.getElementById('someText').innerHTML = age;
+
+ var num1 = 5.7;
+ //numbers in js
+ var num1 = 10;
+ //increment
+ num1 = num1 + 1;
+ console.log(num1);
+
+// another method
+num1++;
+console.log(num1);
+//increment by any number
+num1 += 4;
+console.log(num1);
+/*functions in JS
+
+1.create a function
+2. call the function*/
+
+function fun(){
+    alert('this is a function')
+}
+/* CREATE function that takes in a name\
+ and says hello followed by your name 
+
+  for example 
+  Name: "kay"
+retuturn: "hello kay"
+*/
+function greeting(){
+    var name = prompt('What is your name');
+    var result = 'Hello' + ' ' + name
+    console.log(result);
+
+
+}
+greeting();
+
+
+
+
