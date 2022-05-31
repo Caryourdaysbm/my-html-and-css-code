4fc860ed78935018297f0aa7947b6fbb243667f3
unknown <adeyemioluwakayode73@gmail.com>
2022-05-31 08:21:39 +0100

    task updated

---
+++ Zuri internship/adding intergers.py
@@ -0,0 +1,15 @@
+# Program make a simple calculator
+
+# This function adds two numbers
+def add(x, y):
+    
+
+
+
+        x = float(input("Enter first number: "))
+        y = float(input("Enter second number: "))
+
+        
+            print(x, "+", y, "=", add(x, y))
+
+       
